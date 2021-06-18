77fec8895fb9c72846dacef1792f8388436931c7
Ilya Kotlyakov <m@2ik.ru>
2021-06-18 19:08:03 +0400

    Remove db

--- src/sw.js
+++ src/sw.js
@@ -1,76 +1,4 @@
-var KEYS = {
-  BET: 'lastBet',
-  SHOW: 'lastShow',
-  CLICK: 'lastClick'
-}
-
-var IDB = (function (n) {
-  'use strict'
-
-  function t(n) {
-    return new Promise(function (t, r) {
-      ;(n.oncomplete = n.onsuccess =
-        function () {
-          return t(n.result)
-        }),
-        (n.onabort = n.onerror =
-          function () {
-            return r(n.error)
-          })
-    })
-  }
-
-  function r(n, r) {
-    var e = indexedDB.open(n)
-    e.onupgradeneeded = function () {
-      var b = e.result
-      if (!b.objectStoreNames.contains(r)) {
-        var c = b.createObjectStore(r)
-        return c.add(0, KEYS.BET), c.add(0, KEYS.SHOW), c.add(0, KEYS.CLICK), c
-      }
-    }
-    var u = t(e)
-    return function (n, t) {
-      return u.then(function (e) {
-        return t(e.transaction(r, n).objectStore(r))
-      })
-    }
-  }
-  var e
-
-  function u() {
-    return e || (e = r('resources', 'push')), e
-  }
-
-  function o(n, r) {
-    return n('readonly', function (n) {
-      return (
-        (n.openCursor().onsuccess = function () {
-          this.result && (r(this.result), this.result.continue())
-        }),
-        t(n.transaction)
-      )
-    })
-  }
-  return (
-    (n.get = function (n) {
-      var r = u()
-      return r('readonly', function (r) {
-        return t(r.get(n))
-      })
-    }),
-    (n.set = function (n, r) {
-      var e = u()
-      return e('readwrite', function (e) {
-        return e.put(r, n), t(e.transaction)
-      })
-    }),
-    n
-  )
-})({})
-
 var DEFAULT_CONFIG = {
-  frequencyOfBet: 1200, // 60 * 60
   defaultPush: [
     {
       title: '',
@@ -83,30 +11,11 @@ self.addEventListener('push', event => {
   event.waitUntil(onPush_(event))
 })
 
-self.addEventListener('notificationclick', function (event) {
-  const target = event.notification.data.click_action || '/'
+self.addEventListener('notificationclick', event => {
   event.notification.close()
+  const target = event.notification.data.click_action || '/'
 
-  event.waitUntil(
-    clients
-      .matchAll({
-        type: 'window',
-        includeUncontrolled: true
-      })
-      .then(function (clientList) {
-        // clientList always is empty?!
-        for (var i = 0; i < clientList.length; i++) {
-          var client = clientList[i]
-          if (client.url === target && 'focus' in client) {
-            return client.focus()
-          }
-        }
-
-        IDB.set(KEYS.CLICK, Date.now())
-
-        return clients.openWindow(target)
-      })
-  )
+  event.waitUntil(clients.openWindow(target))
 })
 
 function getDefaultPush_() {
@@ -120,7 +29,7 @@ function defaultPush_() {
   return self.registration.showNotification(push.title, push)
 }
 
-async function canBidRequest_() {
+async function loadConfig_() {
   const randomVersion = Math.random()
   const response = await fetch(
     'https://cdn.jsdelivr.net/gh/adoperator/push_pixel@latest/dist/config.json?v=' +
@@ -130,23 +39,19 @@ async function canBidRequest_() {
   if (response.ok) {
     DEFAULT_CONFIG = await response.json()
   }
-
-  const storeLastBet = parseInt(await IDB.get(KEYS.BET)) || 0
-
-  if (!storeLastBet) {
-    IDB.set(KEYS.BET, 0)
-  }
-
-  return Date.now() - storeLastBet > DEFAULT_CONFIG.frequencyOfBet * 1000
 }
 
 async function onPush_(event) {
-  const canBidRequest = await canBidRequest_()
+  await loadConfig_()
 
-  if (!canBidRequest) {
+  let push
+
+  try {
+    push = event.data.json()
+  } catch (error) {
     return defaultPush_()
   }
-  const push = event.data.json()
+
   const notification = push.notification || {}
 
   if (!notification.body) return defaultPush_()
@@ -155,19 +60,17 @@ async function onPush_(event) {
   const link = notification.body.replace('%user_agent%', ua)
   const response = await fetch(link)
 
-  IDB.set(KEYS.BET, Date.now())
-
   if (!response.ok) return defaultPush_()
 
   const notifications = await response.json()
 
   if (!notifications.length) return defaultPush_()
 
-  await sendPush_(notifications)
+  return await sendPush_(notifications)
 }
 
 async function sendPush_(notifications) {
-  notifications.forEach(async notification => {
+  return notifications.forEach(async notification => {
     await self.registration.showNotification(notification.title, {
       body: notification.text,
       icon: notification.icon_url,
@@ -176,6 +79,5 @@ async function sendPush_(notifications) {
         click_action: notification.click_url
       }
     })
-    IDB.set(KEYS.SHOW, Date.now())
   })
 }
